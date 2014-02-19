d3b873c15e05ae5ca8ffc5e1e18f3aaedf87f29e
Brandon Larsen <brandonlarsen@gmail.com>
2014-02-18 23:16:01 -0700

    index.php page creation

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML> 
+<html lang="en-us"> 
+<head> 
+<meta charset="utf-8"> 
+<title>Food Foraging Fiesta</title> 
+
+</head> 
+
+<body> 
+<?php include "header.php"; ?> 
+<h1>Food Foraging Fiesta</h1>
+<p>Food Storage Inventory Control</p>
+<h2>Login:</h2>
+<p>Email: 
+  <input type="text" name="textfield" id="textfield">
+  <br>
+  Password: <input type="text" name="textfield" id="textfield">
+  <br>
+  <input type="submit" name="submit" id="submit" value="Submit">
+</p>
+<h2>Create Account:</h2>
+<p>Email: <input type="text" name="textfield" id="textfield"><br>
+  Password: <input type="text" name="textfield" id="textfield"><br>
+Password x2: <input type="text" name="textfield" id="textfield">
+<br>
+<input type="submit" name="submit2" id="submit2" value="Submit">
+</p>
+<?php include "footer.php"; ?> 
+</body> 
+</html>
